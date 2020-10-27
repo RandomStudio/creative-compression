dcf01eb75c37d9c19c81d8c67e536d1faf5ff50f
Andrew Hill <andrew.hill@random.studio>
2020-10-27 16:54:15 +0100

    Change output folder

--- docs/examples.e31bb0bc.js
+++ docs/examples.e31bb0bc.js
@@ -9991,7 +9991,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54887" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55577" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
