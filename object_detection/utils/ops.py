e33aae5e6234ec54b87652dc36dfb8e68021a7df
Andrew Hill <andrew.hill@random.studio>
2020-11-25 10:35:47 +0100

    Remove tf_slim

--- object_detection/utils/ops.py
+++ object_detection/utils/ops.py
@@ -25,7 +25,6 @@ import six
 from six.moves import range
 from six.moves import zip
 import tensorflow.compat.v1 as tf
-import tf_slim as slim
 from object_detection.core import standard_fields as fields
 from object_detection.utils import shape_utils
 from object_detection.utils import spatial_transform_ops as spatial_ops
@@ -523,83 +522,6 @@ def filter_unrecognized_classes(tensor_dict):
                  unrecognized_label))
 
   return retain_groundtruth(tensor_dict, recognized_indices)
-
-
-def normalize_to_target(inputs,
-                        target_norm_value,
-                        dim,
-                        epsilon=1e-7,
-                        trainable=True,
-                        scope='NormalizeToTarget',
-                        summarize=True):
-  """L2 normalizes the inputs across the specified dimension to a target norm.
-
-  This op implements the L2 Normalization layer introduced in
-  Liu, Wei, et al. "SSD: Single Shot MultiBox Detector."
-  and Liu, Wei, Andrew Rabinovich, and Alexander C. Berg.
-  "Parsenet: Looking wider to see better." and is useful for bringing
-  activations from multiple layers in a convnet to a standard scale.
-
-  Note that the rank of `inputs` must be known and the dimension to which
-  normalization is to be applied should be statically defined.
-
-  TODO(jonathanhuang): Add option to scale by L2 norm of the entire input.
-
-  Args:
-    inputs: A `Tensor` of arbitrary size.
-    target_norm_value: A float value that specifies an initial target norm or
-      a list of floats (whose length must be equal to the depth along the
-      dimension to be normalized) specifying a per-dimension multiplier
-      after normalization.
-    dim: The dimension along which the input is normalized.
-    epsilon: A small value to add to the inputs to avoid dividing by zero.
-    trainable: Whether the norm is trainable or not
-    scope: Optional scope for variable_scope.
-    summarize: Whether or not to add a tensorflow summary for the op.
-
-  Returns:
-    The input tensor normalized to the specified target norm.
-
-  Raises:
-    ValueError: If dim is smaller than the number of dimensions in 'inputs'.
-    ValueError: If target_norm_value is not a float or a list of floats with
-      length equal to the depth along the dimension to be normalized.
-  """
-  with tf.variable_scope(scope, 'NormalizeToTarget', [inputs]):
-    if not inputs.get_shape():
-      raise ValueError('The input rank must be known.')
-    input_shape = inputs.get_shape().as_list()
-    input_rank = len(input_shape)
-    if dim < 0 or dim >= input_rank:
-      raise ValueError(
-          'dim must be non-negative but smaller than the input rank.')
-    if not input_shape[dim]:
-      raise ValueError('input shape should be statically defined along '
-                       'the specified dimension.')
-    depth = input_shape[dim]
-    if not (isinstance(target_norm_value, float) or
-            (isinstance(target_norm_value, list) and
-             len(target_norm_value) == depth) and
-            all([isinstance(val, float) for val in target_norm_value])):
-      raise ValueError('target_norm_value must be a float or a list of floats '
-                       'with length equal to the depth along the dimension to '
-                       'be normalized.')
-    if isinstance(target_norm_value, float):
-      initial_norm = depth * [target_norm_value]
-    else:
-      initial_norm = target_norm_value
-    target_norm = slim.model_variable(
-        name='weights',
-        dtype=tf.float32,
-        initializer=tf.constant(initial_norm, dtype=tf.float32),
-        trainable=trainable)
-    if summarize:
-      mean = tf.reduce_mean(target_norm)
-      tf.summary.scalar(tf.get_variable_scope().name, mean)
-    lengths = epsilon + tf.sqrt(tf.reduce_sum(tf.square(inputs), dim, True))
-    mult_shape = input_rank*[1]
-    mult_shape[dim] = depth
-    return tf.reshape(target_norm, mult_shape) * tf.truediv(inputs, lengths)
 
 
 def batch_position_sensitive_crop_regions(images,
